1e45ccade9d9c722c2fcf775d0a2062d58dcbe2c
Armando Martinez <alemar.martinez16@gmail.com>
2023-08-24 14:26:21 -0300

    importacion de componentes en App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+//importamos los componentes
+import Create from "./components/Create";
+import Edit from "./components/Edit";
+import Show from "./components/Show";
 
 function App() {
   return <div className="App">
     <h1>Hello World </h1>
     <button className="btn btn-primary">Crear</button>
+    <Show />
   </div>;
 }
 
