3ecbc6a89399bccdcb1feb5f6a502ba2cef021bd
Armando Martinez <alemar.martinez16@gmail.com>
2023-08-24 12:07:34 -0300

    Prueba con bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <h1>Hello World </h1>
+    <button className="btn btn-primary">Crear</button>
+  </div>;
 }
 
 export default App;
