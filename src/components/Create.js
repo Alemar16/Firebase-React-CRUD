4d1c0c18d88efd3ac3b4a96d2b3c6adbe53b0b94
Armando Martinez <alemar.martinez16@gmail.com>
2023-08-24 19:44:16 -0300

    Se construye el componente y se define interfax

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,9 +1,68 @@
-import React from 'react'
+
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "../firebaseConfig/firebase";
 
 const Create = () => {
+  // 1- configuracion de hooks
+  const [description, setDescription] = useState("");
+  const [stock, setStock] = useState(0);
+  const navigate = useNavigate();
+
+  // 2- Referencia de la DB firestore
+  const productsCollection = collection(db, "products");
+
+  // 3- funcion para almacenar
+  const store = async (e) => {
+    e.preventDefault();
+    //console.log(e.target)
+    //console.log(e.target[0].value)
+    await addDoc(productsCollection, { description: description, stock: stock })
+    navigate("/");
+  }
+
+
+
+
   return (
-    <div>Vista de Create</div>
-  )
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1>Create Product</h1>
+
+          {/* formulario */}
+          <form onSubmit={store}>
+
+            {/* input description */}
+            <div className="mb-3">
+              <label className="form-label">Description</label>
+              <input
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                type="text"
+                className="form-control"
+              />
+            </div>
+
+            {/* input stock */}
+            <div className="mb-3">
+              <label className="form-label">Stock</label>
+              <input
+                value={stock}
+                onChange={(e) => setStock(e.target.value)}
+                type="number"
+                className="form-control"
+              />
+            </div>
+
+            {/* boton */}
+            <button type="submit" className="btn btn-primary">Create</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Create
