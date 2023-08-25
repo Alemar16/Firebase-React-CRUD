e1bb550f544181e496d62e3569bc159dad94fa0f
Armando Martinez <alemar.martinez16@gmail.com>
2023-08-24 21:46:36 -0300

    Se define el funcionamiento del componente

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -1,9 +1,81 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { getDoc, updateDoc, doc } from "firebase/firestore";
+import { db } from "../firebaseConfig/firebase";
 
 const Edit = () => {
-  return (
-    <div>vista de Edit</div>
-  )
-}
+  // 1- configuracion de hooks
+  const [description, setDescription] = useState("");
+  const [stock, setStock] = useState(0);
 
-export default Edit
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  // 2- creamos las funciones para obtener el documento y actualizarlo
+  const update = async (e) => {
+    e.preventDefault();
+    const product = doc(db, "products", id);
+    const data = { description: description, stock: stock };
+    await updateDoc(product, data);
+    navigate("/");
+  };
+
+  const getProductsById = async (id) => {
+    const product = await getDoc(doc(db, "products", id));
+    if (product.exists()) {
+      //console.log(product.data());
+      setDescription(product.data().description);
+      setStock(product.data().stock);
+    } else {
+      console.log("No such product!");
+    }
+  };
+
+  // 3- usamos useEffect
+  useEffect(() => {
+    getProductsById(id);
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1>Edit Product</h1>
+
+          {/* formulario */}
+          <form onSubmit={update}>
+            {/* input description */}
+            <div className="mb-3">
+              <label className="form-label">Description</label>
+              <input
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                type="text"
+                className="form-control"
+              />
+            </div>
+
+            {/* input stock */}
+            <div className="mb-3">
+              <label className="form-label">Stock</label>
+              <input
+                value={stock}
+                onChange={(e) => setStock(e.target.value)}
+                type="number"
+                className="form-control"
+              />
+            </div>
+
+            {/* boton */}
+            <button type="submit" className="btn btn-primary">
+              Update
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Edit;
